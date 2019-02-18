a276c82e7348d3b1cef5387e7f453ebabf3e4b65
Bjorkbat <bjorkbat@gmail.com>
2019-02-18 10:53:27 -0500

    First commit, ready to go with uikit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import Vuikit from 'vuikit';
+import VuikitIcons from '@vuikit/icons';
 import App from './App.vue'
 
+// Linter will complain about this, don't worry
+import VuikitTheme from '@vuikit/theme';
+
 Vue.config.productionTip = false
+
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
 
 new Vue({
   render: h => h(App),
