4dc13f2440e78fa5353d9428b30e515bb48db884
Bjorkbat <bjorkbat@gmail.com>
2019-02-18 16:55:35 -0500

    Remove vuikit icons

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import Vuikit from 'vuikit';
-import VuikitIcons from '@vuikit/icons';
 // eslint-disable-next-line
 import VuikitTheme from '@vuikit/theme';
 
 import App from './App.vue'
 
-
 Vue.config.productionTip = false
 
 Vue.use(Vuikit);
-Vue.use(VuikitIcons);
 
 new Vue({
   render: h => h(App),
