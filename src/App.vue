d9184c83f37066eb1ff77b837e55d3d741779c6b
Bjorkbat <bjorkbat@gmail.com>
2019-02-18 17:58:21 -0500

    fleshed out how it works page

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,22 @@
     <Hero />
 
     <FoldIcons />
+
+    <HowItWorks />
   </div>
 </template>
 
 <script>
 import FoldIcons from './components/FoldIcons.vue';
 import Hero from './components/Hero.vue';
+import HowItWorks from './components/HowItWorks.vue';
 import Navbar from './components/Navbar.vue';
 export default {
   name: 'app',
   components: {
     FoldIcons,
     Hero,
+    HowItWorks,
     Navbar
   }
 }
---
+++ src/components/HowItWorks.vue
@@ -0,0 +1,80 @@
+<!-- Component for rendering the how it works section -->
+<template>
+	<div class="how-it-works">
+		<h1 class="uk-text-center">How it works</h1>
+
+		<vk-grid gutter="medium" class="uk-child-width-expand@m" matched>
+
+			<div>
+				<vk-card type="blank">
+					<div class="foo">
+						<img src="../assets/circle-icon-comment.png"/>
+						<h4 class="uk-text-uppercase">Review Your Finances</h4>
+					</div>
+					<p class="uk-text-lead">Review your debts, budget and credit free of
+						charge with a certified Credit counselor.</p>
+				</vk-card>
+			</div>
+
+			<div>
+				<vk-card type="blank">
+					<div class="foo">
+						<img src="../assets/circle-icon-gesture.png"/>
+						<h4 class="uk-text-uppercase">We start when you are ready</h4>
+					</div>
+					<p class="uk-text-lead">Once you are ready to go, we will contact
+						your creditors to negotiate lower interest rates.</p>
+				</vk-card>
+			</div>
+
+			<div>
+				<vk-card type="blank">
+					<div class="foo">
+						<img src="../assets/circle-icon-dollar.png"/>
+						<h4 class="uk-text-uppercase">Get out of debt faster</h4>
+					</div>
+					<p class="uk-text-lead">Debt-free as soon as 36 months by one monthly
+						payment to us.  We take care of the rest.</p>
+				</vk-card>
+			</div>
+
+		</vk-grid>
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HowItWorks"
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'src/styles/colors';
+.how-it-works {
+	padding: 160px 0;
+	margin: 20px;
+
+	.foo {
+		display: flex;
+		align-items: center;
+
+		& h4 {
+			width: 100%;
+			flex-grow: 1;
+		}
+
+		& img {
+			margin-right: 20px;
+		}
+	}
+}
+
+.uk-text-lead {
+	font-size: 20px;
+}
+
+.uk-card {
+	border: 2px solid $light-grey;
+}
+</style>
