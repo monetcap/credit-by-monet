3ceb8b24cc991ad29a3f78cc778e6e149571bcb7
Bjorkbat <bjorkbat@gmail.com>
2019-02-19 16:29:29 -0500

    Started work on slider form

---
+++ src/components/slider-form/HowSoon.vue
@@ -0,0 +1,23 @@
+<!-- This component renders the how soon section of the slider form -->
+<template>
+
+	<div>
+		<h2>How soon would you like to pay off ${{ amount }}</h2>
+
+		<form>
+			<label><input type="radio" class="uk-radio" name="how-soon"/>3 years from now.</label>
+			<label><input type="radio" class="uk-radio" name="how-soon"/>3 years from now.</label>
+			<label><input type="radio" class="uk-radio" name="how-soon"/>3 years from now.</label>
+			<label><input type="radio" class="uk-radio" name="how-soon"/>3 years from now.</label>
+		</form>
+
+	</div>
+
+</template>
+
+<script>
+export default {
+	name: "HowSoon",
+	props: ['amount']
+}
+</script>
---
+++ src/components/slider-form/SliderForm.vue
@@ -0,0 +1,27 @@
+<template>
+
+	<vk-modal :show.sync="show" size="container">
+
+		<vk-modal-close @click="$emit('update:show', false)"></vk-modal-close>
+
+		<vk-tabs-vertical align="left">
+
+			<vk-tabs-item title="$45,000">Foo!</vk-tabs-item>
+			<vk-tabs-item title="How Soon">
+				<HowSoon amount="45,000"/>
+			</vk-tabs-item>
+
+		</vk-tabs-vertical>
+
+	</vk-modal>
+
+</template>
+
+<script>
+import HowSoon from './HowSoon.vue';
+export default {
+	name: "SliderForm",
+	components: { HowSoon },
+	props: ['show']
+}
+</script>
