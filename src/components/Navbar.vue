53f520e84761ec235f665e68db1f6a094c621dc1
Bjorkbat <bjorkbat@gmail.com>
2019-02-20 17:29:09 -0500

    Navbar is pretty much good to go

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,58 @@
-<!-- Component responsible for rendering the site's navbar -->
+<!-- Component responsible for rendering the site's navbar.  Also renders a
+	contact modal if the user elects to click the contact button -->
 <template>
-	<vk-navbar>
-	</vk-navbar>
+
+	<vk-sticky>
+
+		<div>
+
+			<ContactModal :show.sync="show" title="Contact Us" class="bg-white"/>
+
+			<vk-navbar>
+
+				<vk-navbar-nav>
+
+					<a href="#app" v-vk-scroll>
+						<vk-navbar-logo>
+								<img src="../assets/logo.png"/>
+						</vk-navbar-logo>
+					</a>
+
+				</vk-navbar-nav>
+
+				<vk-navbar-nav slot="right">
+					<vk-navbar-item>
+						<vk-button type="secondary" @click="show = true">Contact Us</vk-button>
+					</vk-navbar-item>
+				</vk-navbar-nav>
+
+			</vk-navbar>
+
+		</div>
+
+	</vk-sticky>
+
 </template>
 
 <script>
+import ContactModal from './contact/ContactModal.vue';
 export default {
-	name: "Navbar"
+	name: "Navbar",
+	components: { ContactModal },
+	data: function() {
+		return {
+			show: false
+		}
+	}
 }
 </script>
+
+<style lang="scss" scoped>
+nav.uk-navbar {
+	background-color: rgba(248, 248, 248, 0.95);
+}
+img {
+	max-height: 100%;
+	padding: 20px 0;
+}
+</style>
