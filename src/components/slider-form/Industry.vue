5e0d5b9e14a902fc9ee0aadd684324ab0e2a59d0
Bjorkbat <bjorkbat@gmail.com>
2019-02-28 08:16:56 -0500

    Added insdustries to slider

--- src/components/slider-form/Industry.vue
+++ src/components/slider-form/Industry.vue
@@ -3,8 +3,30 @@
 <template>
 	<div>
 		<h2 class="uk-text-uppercase">What industry are you in?</h2>
+
+		<select class="uk-select">
+			<option>Accommodation & Food Services</option>
+			<option>Agriculture, Forestry, and Fishing</option>
+			<option>Arts, Entertainment, and Recreation</option>
+			<option>Construction</option>
+			<option>Educational Services</option>
+			<option>Finance and Insurance</option>
+			<option>Health Care & Social Assistance</option>
+			<option>Manufacturing</option>
+			<option>Professional, Scientific & Technical</option>
+			<option>Services</option>
+			<option>Real Estate, Rental & Leasing</option>
+			<option>Retail Trade</option>
+			<option>Transportation and Warehousing</option>
+			<option>Wholesale Trade</option>
+			<option>Other (please specify)</option>
+		</select>
+
 	</div>
 </template>
 
 <script>
+export default {
+	name: "Industry"
+}
 </script>
