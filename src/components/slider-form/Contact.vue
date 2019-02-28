218650cf559089462d96f16b6c1268c5d69bbb55
Bjorkbat <bjorkbat@gmail.com>
2019-02-28 12:49:35 -0500

    Did a rename, added an h2

--- src/components/slider-form/Contact.vue
+++
@@ -1,6 +0,0 @@
-<!-- Component for rendering a contact form that the user can use to enter
-	contact info -->
-<template>
-	<div>
-	</div>
-</template>
---
+++ src/components/slider-form/SliderContact.vue
@@ -0,0 +1,14 @@
+<!-- Component for rendering a contact form that the user can use to enter
+	contact info.  Unlike shared contact form used in other pages, it has a
+	simpler layout, which prompted the creation of another component. -->
+<template>
+	<div>
+		<h2 class="uk-text-uppercase">How Can We Contact You?</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "SliderContact"
+}
+</script>
