b7d2a07895fc62b004561bb739d03f278c708a87
Bjorkbat <bjorkbat@gmail.com>
2019-04-12 13:57:57 -0600

    Started a general information section, didn't get far

---
+++ src/components/slider-form/GeneralInformation.vue
@@ -0,0 +1,20 @@
+<!-- Portion of modal for collecting, you guessed it, general information about
+	the applicant -->
+<template>
+	<div>
+		<h2 class="text-gold semi-bold uk-text-uppercase">We'll help you access
+			{{ amount }}?</h2>
+		<h3>Provide your details below to get started</h3>
+
+		<form>
+			<vk-grid>
+			</vk-grid>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "GeneralInformation"
+}
+</script>
