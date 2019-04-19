97e7d690be66d3bcf547dc698e420d5ef9cb75b7
Ariella Hylas <ari.saves@gmail.com>
2019-04-18 19:37:50 -0600

    Adding copy for terms, should prob put this kinda thing in a strings file or api doc.

--- src/components/slider-form/SliderForm.vue
+++ src/components/slider-form/SliderForm.vue
@@ -25,17 +25,48 @@
 
         <vk-tabs-item title="Page2">
           <h4>Consumer Credit File Rights (State & Federal)</h4>
-          <textarea class="uk-textarea uk-margin-small-bottom" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+          <div style="overflow-y: scroll; height: 8em;"class="uk-textarea uk-margin-small-bottom">
+            <p>You have a right to dispute inaccurate information in your credit report by contacting the credit bureau directly. However, neither you nor any ‘credit repair’ company or credit repair organization has the right to have accurate, current, and verifiable information removed from your credit report. The credit bureau must remove accurate, negative information from your report only if it is over 7 years old.
+            Bankruptcy information can be reported for 10 years.</p>
+            <p>You have a right to obtain a copy of your credit report from a credit bureau. You may be charged a reasonable fee. There is no fee, however, if you have been turned down for credit, employment, insurance, or a rental dwelling because of information in your credit report within the
+            preceding 60 days. The credit bureau must provide someone to help you interpret the information in your credit file.</p>
+            <p>You are entitled to receive a free copy of your credit report if you are unemployed and intend to apply for employment in the next 60 days, if you are a recipient of public welfare assistance, or if you have reason to believe that there is inaccurate information in your credit report due to fraud.
+            You have a right to sue a credit repair organization that violates the Credit Repair Organization Act. This law prohibits deceptive practices by credit repair organizations.</p>
+            <p>You have the right to cancel your contract with any credit repair organization for any reason within 3 business days from the date you signed it.</p>
+            <p>Credit bureaus are required to follow reasonable procedures to ensure that the information they report is accurate. However, mistakes may occur.</p>
+            <p>You may, on your own, notify a credit bureau in writing that you dispute the accuracy of information in your credit file. The credit bureau must then reinvestigate and modify or
+            remove inaccurate or incomplete information. The credit bureau may not charge any fee for this service. Any pertinent information and copies of all documents you have concerning an error should be given to the credit bureau.</p>
+            <p>If the credit bureau’s reinvestigation does not resolve the dispute to your satisfaction, you may send a brief statement to the credit bureau, to be kept in your file, explaining why you think the record is inaccurate. The credit bureau must include a summary of your statement about disputed information with any report it issues about you.</p>
+            <p>The Federal Trade Commission regulates credit bureaus and credit repair organizations.<p>
+            <p><b>For more information contact:</b><br>
+            The Public Reference Branch<br>
+            Federal Trade Commission<br>Washington, D.C. 20580</p>
+          </div>
+          <input id="rights-checkbox" class="uk-checkbox uk-margin-small-right" type="checkbox" v-model="rightsCheckbox" />
           <label for="rights-checkbox">I have read and understand my rights.</label>
-          <input id="rights-checkbox" class="uk-checkbox uk-margin-small-left" type="checkbox" v-model="rightsCheckbox" />
-
           <hr />
 
           <h4 class="uk-margin-remove-top">E-Sign Consent and Disclosure</h4>
-          <textarea class="uk-textarea uk-margin-small-bottom" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+          <div style="overflow-y: scroll; height: 8em;"class="uk-textarea uk-margin-small-bottom">
+            <p>
+              <b>Please carefully review this E-Sign Consent and Disclosure Form in order to use your electronic signa- ture and receive all
+              communications electronically. Be advised that your consent is optional.</b>
+            </p>
+            <p>
+              <b>Your Consent to Use Electronic Signature and Receive Electronic Communications.</b>
+              By signing this form, you agree that your e-signature shall be deemed your legal signature; you consent to the use of your electronic signature for the purpose of signing documents, and to receive documents and/ or communications (“Communication(s)”) related to Credit Repair Source LLC d/b/a Credit by Monet (“Mon- et”) services electronically unless and until you withdraw your consent as described herein. Your consent includes, but is not limited to the following communications:
+              <ul>
+                <li>notices</li>
+                <li>disclosures</li>
+                <li>consents</li>
+                <li>contracts</li>
+                <li>any communications with respect to Monet, credit bureau and/or any communication relating to your credit.</li>
+              </ul>
+              By agreeing, you consent that all Communications that Monet will provide to you in electronic form will be sent to the email you indicated on file.
+            </p>
+          </div>
+          <input id="esign-checkbox" class="uk-checkbox uk-margin-small-right" type="checkbox" v-model="esignCheckbox" />
           <label for="esign-checkbox">I have read the disclosure and consent to the terms.</label>
-          <input id="esign-checkbox" class="uk-checkbox uk-margin-small-left" type="checkbox" v-model="esignCheckbox" />
-
         </vk-tabs-item>
 
         <vk-tabs-item title="Page3">
